5871624df79c0dc87fa5df75a080613a23d6f3b7
adelabria <adelabria@students.amikom.ac.id>
2024-03-10 23:09:17 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu;
+            float a, b;
+
+            Console.WriteLine("Pilih menu calculator");
+            Console.WriteLine("1. Penambahan");
+            Console.WriteLine("2. Pengurangan");
+            Console.WriteLine("3. Perkalian");
+            Console.WriteLine("4. Pembagian");
+            Console.WriteLine("Input nomor menu [1..4]: ");
+            menu = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Inputkan nilai a = ");
+            a = float.Parse(Console.ReadLine());
+            Console.Write("Inputkan nilai b = ");
+            b = float.Parse(Console.ReadLine());
+
+            if (menu == 1)
+            {
+                Console.WriteLine($"Hasil penambahan {a} + {b} = {a + b}");
+            }
+            else if (menu == 2)
+            {
+                Console.WriteLine($"Hasil penambahan {a} - {b} = {a - b}");
+            }
+            else if (menu == 3)
+            {
+                Console.WriteLine($"Hasil penambahan {a} * {b} = {a * b}");
+            }
+            else if (menu == 4)
+            {
+                if (b == 0) 
+                {
+                    Console.WriteLine("Tidak dapat melakukan pembagian dengan nol");
+                }
+                else
+                {
+                    Console.WriteLine($"Hasil penambahan {a} / {b} = {a / b}");
+                }
+               
+            }
+            else
+            {
+                Console.WriteLine("Input tidak valid");
+            }
+
+            Console.WriteLine("Tekan sembarang key untuk keluar");
+            Console.ReadKey();
+        }
+    }
+}
